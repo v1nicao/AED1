cb31f4f230f4bd3bfccd8502d6a446102db56449
Vinicius da silva <106478122+v1nicao@users.noreply.github.com>
2023-05-16 21:23:14 -0300

    Update 4.c

--- Recursão/4.c
+++ Recursão/4.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
-
-unsigned int duploFatorial(int n){
+ 
+unsigned long int duploFatorial(long int n){
     if(n<=1) return 1;
-
-    return n * duploFatorial(n-2);
+ 
+    /*
+    if(n%2 == 0) return n * duploFatorial(n-1);
+    else return n*duploFatorial(n-2);
+    */
+    return n*duploFatorial(n-2);
 }
-
+ 
 int main(){
-    unsigned int num;
+    unsigned long long int num;
+ 
+    //printf("Digite um numero para calcular o fatorial duplo:\n");
+    scanf("%llu", &num);
+    
+    if(num >= 1 && num <=100){
+        if(num%2 == 0){
+            num--;
+            //duploFatorial(num);
+        }
+        duploFatorial(num);
+        unsigned long long int resultado = duploFatorial(num);
+        printf("%llu\n", resultado);
 
-    printf("Digite um numero para calcular o fatorial duplo:\n");
-    scanf("%u", &num);
-
-    duploFatorial(num);
-    printf("%u\n", duploFatorial(num));
-
+    }
+ 
     return 0;
 }
